3a4ec812cee999c799867d2b5f25494e39517e3c
CN-gzw <114788670+CN-gzw@users.noreply.github.com>
2022-10-24 22:29:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -130,6 +130,13 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
     love_date = date(love_year, love_month, love_day)
     # 获取在一起的日期差
     love_days = str(today.__sub__(love_date)).split(" ")[0]
+    
+    meet_year = int(config["meet_date"].split("-")[0])
+    meet_month = int(config["meet_date"].split("-")[1])
+    meet_day = int(config["meet_date"].split("-")[2])
+    meet_date = date(meet_year, meet_month, meet_day)
+    # 获取在一起的日期差
+    meet_days = str(today.__sub__(meet_date)).split(" ")[0]
     # 获取所有生日数据
     birthdays = {}
     for k, v in config.items():
@@ -163,6 +170,10 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             },
             "love_day": {
                 "value": love_days,
+                "color": get_color()
+            },
+          "love_day": {
+                "value": meet_days,
                 "color": get_color()
             },
             "note_en": {
